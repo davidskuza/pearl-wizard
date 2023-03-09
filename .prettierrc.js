2f7899052b8f8794956c492c9513a14d9fdb7bf7
David Skuza <dawid.skuza@gmail.com>
2023-03-09 23:05:29 +0100

    Setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+// Documentation for this file: https://prettier.io/en/configuration.html
+module.exports = {
+  // We use a larger print width because Prettier's word-wrapping seems to be tuned
+  // for plain JavaScript without type annotations
+  printWidth: 120,
+
+  // Use .gitattributes to manage newlines
+  endOfLine: "auto",
+
+  // Use single quotes instead of double quotes
+  singleQuote: false,
+
+  // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
+  // to use them for arrays only
+  trailingComma: "none",
+
+  plugins: [
+    "./common/autoinstallers/rush-prettier/node_modules/prettier-plugin-packagejson",
+    "./common/autoinstallers/rush-prettier/node_modules/prettier-plugin-properties"
+  ]
+};
